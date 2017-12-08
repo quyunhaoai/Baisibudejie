38cac0324ef5d3d5cce795dd70d1eae481e173c0
quyunhaoai <quyunhaoai@163.com>
2017-12-08 14:52:22 +0800

    code

---
+++ BuDeJie/Classes/Essence(精华)/Controller/XMGWordViewController.m
@@ -0,0 +1,75 @@
+//
+//  XMGWordViewController.m
+//  BuDeJie
+//
+//  Created by xiaomage on 16/3/18.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "XMGWordViewController.h"
+
+@interface XMGWordViewController ()
+
+@end
+
+@implementation XMGWordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = XMGRandomColor;
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(XMGNavMaxY + XMGTitlesViewH, 0, XMGTabBarH, 0);
+    self.tableView.scrollIndicatorInsets = self.tableView.contentInset;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tabBarButtonDidRepeatClick) name:XMGTabBarButtonDidRepeatClickNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(titleButtonDidRepeatClick) name:XMGTitleButtonDidRepeatClickNotification object:nil];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark - 监听
+/**
+ *  监听tabBarButton重复点击
+ */
+- (void)tabBarButtonDidRepeatClick
+{
+    //    if (重复点击的不是精华按钮) return;
+    if (self.view.window == nil) return;
+    
+    //    if (显示在正中间的不是AllViewController) return;
+    if (self.tableView.scrollsToTop == NO) return;
+    
+    XMGLog(@"%@ - 刷新数据", self.class)
+}
+
+/**
+ *  监听titleButton重复点击
+ */
+- (void)titleButtonDidRepeatClick
+{
+    [self tabBarButtonDidRepeatClick];
+}
+
+#pragma mark - 数据源
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 30;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ID = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        cell.backgroundColor = [UIColor clearColor];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%@-%zd", self.class, indexPath.row];
+    return cell;
+}
+
+@end
---
+++ BuDeJie/Classes/Essence(精华)/View/XMGTopicVoiceView.m
@@ -0,0 +1,49 @@
+//
+//  XMGTopicVoiceView.m
+//  BuDeJie
+//
+//  Created by xiaomage on 16/3/24.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "XMGTopicVoiceView.h"
+#import "XMGTopic.h"
+#import <UIImageView+WebCache.h>
+#import <AFNetworking.h>
+
+@interface XMGTopicVoiceView()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *playcountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *voicetimeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *placeholderView;
+@end
+
+@implementation XMGTopicVoiceView
+
+- (void)awakeFromNib
+{
+    self.autoresizingMask = UIViewAutoresizingNone;
+}
+
+- (void)setTopic:(XMGTopic *)topic
+{
+    _topic = topic;
+    
+    // 设置图片
+    self.placeholderView.hidden = NO;
+    [self.imageView xmg_setOriginImage:topic.image1 thumbnailImage:topic.image0 placeholder:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+        if (!image) return;
+        
+        self.placeholderView.hidden = YES;
+    }];
+    
+    // 播放数量
+    if (topic.playcount >= 10000) {
+        self.playcountLabel.text = [NSString stringWithFormat:@"%.1f万播放", topic.playcount / 10000.0];
+    } else {
+        self.playcountLabel.text = [NSString stringWithFormat:@"%zd播放", topic.playcount];
+    }
+    // %04d : 占据4位，多余的空位用0填补
+    self.voicetimeLabel.text = [NSString stringWithFormat:@"%02zd:%02zd", topic.voicetime / 60, topic.voicetime % 60];
+}
+@end
